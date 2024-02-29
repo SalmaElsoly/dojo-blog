6e000ed61e9e3e6cad3b49b0b8631a229c328346
Salma Elsoly <salmaelsoly@gmail.com>
2024-02-29 13:49:05 +0200

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+import BlogList from "../components/BlogList";
+
+const Home = () => {
+  const [blogs, setBlogs] = useState(null);
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
+
+  useEffect(() => {
+    fetch("http://localhost:8000/blogs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+          console.log(data);
+          setBlogs(data);
+      });
+  }, []);
+
+  return (
+    <div className="home">
+          { blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />}
+    </div>
+  );
+};
+
+export default Home;
